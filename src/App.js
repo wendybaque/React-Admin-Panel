9b9abebab5a16728eaba35fa5cffe570a561c73c
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-01-17 14:40:41 +0100

    Add react-router-dom package and routes in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+import List from "./pages/list/List.jsx";
+import Single from "./pages/single/Single.jsx";
+import New from "./pages/new/New.jsx";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="users">
+              <Route index element={<List />}></Route>
+              <Route path=":userId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+            <Route path="products">
+              <Route index element={<List />}></Route>
+              <Route path=":productId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
