8320db8a0dad1865b7b98d7db09f247515eaf177
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-01-18 15:41:00 +0100

    Add and stylize widgets components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 import "./home.scss"
+
 
 const Home = () => {
   return <div className="home">
     <Sidebar/>
     <div className="homecontainer">
       <Navbar />
-      Home container
+      <div className="widgets">
+        <Widget type="user"/>
+        <Widget type="order"/>
+        <Widget type="earning"/>
+        <Widget type="balance"/>
+      </div>
     </div>
   </div>;
 };
